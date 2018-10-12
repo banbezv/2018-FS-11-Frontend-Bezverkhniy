7fc058f41390c9094e9c29c1db53580c574e203f
Bezverkhniy Andrey <banbezv@mail.ru>
2018-10-13 01:50:50 +0300

    Style fixed
    Style for div block is now in style.css file instead of js code

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,7 +1,7 @@
 const say = function (text) {
 	var div=document.createElement('div');
-	div.style.class='colored_block';
-	//div.innerHTML=text;
+	div.classList.add('colored_block');
+	div.innerHTML=text;
 	document.body.appendChild(div);
 };
 
