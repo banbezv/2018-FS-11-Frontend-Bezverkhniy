0b3dac9e7ce52a6d2d08d6f099faca7d9ba00971
Bezverkhniy Andrey <banbezv@mail.ru>
2018-10-14 13:26:24 +0300

    ESLint added

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,9 +1,8 @@
-const say = function (text) {
-	var div=document.createElement('div');
-	div.classList.add('colored_block');
-	div.innerHTML=text;
-	document.body.appendChild(div);
+const say = function say(text) {
+  const div = document.createElement('div');
+  div.classList.add('colored_block');
+  div.innerHTML = text;
+  document.body.appendChild(div);
 };
 
 export default say;
-
